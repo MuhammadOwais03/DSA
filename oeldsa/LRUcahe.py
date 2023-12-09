900e8b72010158a32d34f4acb21fb425770ff80c
Muhammad Owais <138030370+MuhammadOwais03@users.noreply.github.com>
2023-12-09 19:43:50 +0500

    Add files via upload

---
+++ oeldsa/LRUcahe.py
@@ -0,0 +1,37 @@
+from qUeue import *
+
+class LRU:
+    def __init__(self, elements):
+        self.lru = queue(elements)
+        self.length = elements
+        
+
+    def put(self,key, value):
+        elements, track = self.lru.getter()
+        if track == self.length-1:
+             self.lru.pop()
+        if track > -1:
+                for k, instance in elements:
+                        if key == k:
+                            self.lru.delete([k,instance])
+        self.lru.push([key, value])
+
+    def get(self, key):
+         elements, track = self.lru.getter()
+         flag = False
+         if track >-1:
+            for k, val in elements:
+                if k == key:
+                    flag = True
+                    #ind = elements.index([k,val])
+                    instance = [k,val]
+                    self.lru.delete(instance)      
+            if flag:
+                 self.lru.push(instance)
+                 return instance[1]
+            else:
+                 return -1
+    def __str__(self):
+        elements, track = self.lru.getter()
+        return str(elements)
+    
---
+++ oeldsa/test.py
@@ -0,0 +1,15 @@
+from LRUcahe import *
+
+l = LRU(2)
+l.put(1,1)
+l.put(2,2)
+print("Value Of key 1: ", l.get(1))
+l.put(3,3)
+l.put(4,4)
+print("Value of Key 3: ", l.get(3))
+print("Value Of Key 4: ", l.get(4))
+print("Value Of Key 5: ", l.get(5))
+
+
+
+print(l)
